045473e139c4c151cee6acbe6383adecd62c4aa5
Mizzoro <misbah.mukith@outlook.com>
2023-09-13 23:57:48 +0100

    completed layout of portfolio

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Computer Science Student", "Full-Stack developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+    loopCount: Infinity
+})
+
+
